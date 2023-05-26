5241ce325a31b4df2064b5936b3ce9d90341960a
Mohammed Bin Laswad <127851576+M-Laswad@users.noreply.github.com>
2023-05-26 19:09:42 +0300

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Web3_hw2.Models;
+
+namespace Web3_hw2.Controllers;
+
+public class HomeController : Controller
+{
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+[HttpPost]
+    public IActionResult Calculate(double no1, double no2, string op)
+    {
+        double result;
+
+        switch (op)
+        {
+            case "+":
+                result = no1 + no2;
+                break;
+            case "-":
+                result = no1 - no2;
+                break;
+            case "*":
+                result = no1 * no2;
+                break;
+            case "/":
+                result = no2 == 0 ? 0 : no1 / no2;
+                break;
+            default:
+                result = 0;
+                break;
+        }
+
+        ViewBag.Result = result;
+
+        return View("Index");
+    }
+
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Web3_hw2.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
